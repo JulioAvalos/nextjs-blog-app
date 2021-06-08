c35295b9509f0a9ab801d262ca3c7266343d7c10
JulioAvalos <julioavalos2@msn.com>
2021-06-07 19:35:44 -0600

    adding the "hero" component

---
+++ components/home-page/hero.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import classes from "./hero.module.css";
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/author.png"
+          alt="An image showing the author"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hey there</h1>
+      <p>
+        I blog about web development - especially frontend and backend frameworks like
+        Angular, React, Express, Spring Boot
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
