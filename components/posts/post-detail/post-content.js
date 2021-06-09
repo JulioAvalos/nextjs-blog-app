07b658390440eccc79f9b8a7b8e75386f5a6e321
JulioAvalos <julioavalos2@msn.com>
2021-06-09 01:13:12 -0600

    rendering markdown as JSX

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,5 +1,6 @@
-import PostHeader from './post-header';
+import ReactMarkdown from 'react-markdown';
 
+import PostHeader from './post-header';
 import classes from './post-content.module.css';
 
 const DUMMY_POST = {
@@ -18,7 +19,9 @@ function PostContent() {
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      {DUMMY_POST.content}
+      <ReactMarkdown>
+        {DUMMY_POST.content}
+      </ReactMarkdown>
     </article>
   );
 }
