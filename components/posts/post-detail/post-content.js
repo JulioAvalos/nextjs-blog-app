c1b2eefd0852ba17cb8c4961e80da2c9d98893b5
JulioAvalos <julioavalos2@msn.com>
2021-06-09 19:47:36 -0600

    adding Markdown files as a Data Source

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -7,8 +7,6 @@ const DUMMY_POST = {
   slug: "getting-started-with-nextjs",
   title: "Getting Started with NextJS",
   image: "getting-started-nextjs.png",
-  excerpt:
-    "NextJS is a React framework for production - it makes building fullstack React apps and site a breeze and ships with built-in SSR.",
   date: "2022-02-10",
   content: "# This is a first post",
 };
---
+++ posts/getting-started-with-nextjs.md
@@ -0,0 +1,11 @@
+---
+title: 'Getting Started with NextJS'
+date: '2022-10-15'
+image: getting-started-nextjs.png
+excerpt: NextJS is a React framework for production - it makes building fullstack React apps and site a breeze and ships with built-in SSR.
+isFeatured: true
+---
+
+# This is a title
+
+This is some regultar text with a [link](http://www.google.com)
