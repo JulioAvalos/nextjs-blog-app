865dbfb2060c5827b94311de69aabc408fd3b6c1
JulioAvalos <julioavalos2@msn.com>
2021-06-10 23:46:38 -0600

    Adding "head" Data

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import Head from "next/head";
 
 import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from "../components/home-page/hero";
@@ -8,6 +9,13 @@ import { getFeaturedPosts } from "../lib/posts-util";
 function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>Welcome to my blog</title>
+        <meta
+          name="description"
+          content="I post about programming and web development"
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
@@ -20,7 +28,7 @@ export async function getStaticProps() {
   return {
     props: {
       posts: featuredPosts,
-    }
+    },
   };
 }
 
